5a6e982c0490aeb56a4451f86f8c8926be714c2b
Muhammad Hassan <muhammad.hassan@secp.gov.pk>
2022-04-20 14:12:05 +0500

    modified attorney details

--- src/app/components/settings/app.settings.ts
+++ src/app/components/settings/app.settings.ts
@@ -13,3 +13,66 @@ export const IMAGES = [
     'assets/img/clients/petro.jpg',
     'assets/img/clients/amson.png'
 ];
+
+export const LAWYER_DETAILS = [
+    {
+        name: 'Malik Khalid Mehmood Walayet',
+        title: 'Founding Partner',
+        education: 'LLB University of the Punjab'
+    },
+    {
+        name: 'Malik Saqib Mehmood Khalid (AHC)',
+        title: 'Partner',
+        education: 'LLB International Islamic University Islamabad'
+    },
+    {
+        name: 'Mr. Muhammad Usama Rauf',
+        title: 'Partner',
+        education: 'LLB, LLM & Bar at Law UK'
+    },
+    {
+        name: 'Mr. Waqar A. Sheikh (ASC)',
+        title: 'Senior Consultant',
+        education: 'LLB University of the Punjab'
+    },
+    {
+        name: 'Mr. Nadeem Sheikh (ASC)',
+        title: 'Senior Consultant',
+        education: 'LLB University of the Punjab'
+    },
+    {
+        name: 'Mr. Mian Abdul Rauf (ASC)',
+        title: 'Senior Consultant',
+        education: 'LLB University of the Punjab'
+    },
+    {
+        name: 'Mr. Khawar Munir (AHC)',
+        title: 'Associate Partner',
+        education: 'LLB University of the Punjab'
+    },
+    {
+        name: 'Mrs. Misbah Ashaq (AHC)',
+        title: 'Associate Partner',
+        education: 'LLB International Islamic University Islamabad, LLM Qatar'
+    },
+    {
+        name: 'Mr. Adil Mehmood Mirza (AHC)',
+        title: 'Associate Partner',
+        education: 'LLB International Islamic University Islamabad'
+    },
+    {
+        name: 'Miss Saqiba Basharat (AHC)',
+        title: 'Associate Partner',
+        education: 'LLB (Hons) International Islamic University Islamabad'
+    },
+    {
+        name: 'Mr. Malik Shahzaib (AHC)',
+        title: 'Associate Partner',
+        education: 'LLB, LLM & Bar at Law UK'
+    },
+    {
+        name: 'Mr. Ali Irtaza (AHC)',
+        title: 'Associate Partner',
+        education: 'LLB International Islamic University Islamabad'
+    }
+]
